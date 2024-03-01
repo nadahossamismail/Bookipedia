d868dcbaae52188cf36b9b9f6c47e98b777279f4
Nada Hossam Ismail <nadahossamismail@gmail.com>
2024-03-01 22:00:54 +0200

    arch

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,21 @@
-
 import 'package:bookipedia/app/app_routes.dart';
 import 'package:bookipedia/app/style/app_theme.dart';
 import 'package:bookipedia/presentation_layer/views/login_view.dart';
 import 'package:flutter/material.dart';
 
-
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp._internal();
 
-  static const  MyApp _instance =  MyApp._internal();
-  factory MyApp()=> _instance;
-  
+  static const MyApp _instance = MyApp._internal();
+  factory MyApp() => _instance;
+
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeManger().getApplicationTheme(),
-    onGenerateRoute:RouteGenerator.generateRoute ,
-    home:const LoginView(),
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeManger().getApplicationTheme(),
+      onGenerateRoute: RouteGenerator.generateRoute,
+      home: const LoginView(),
     );
   }
 }
--- lib/presentation_layer/views/login_view.dart
+++ lib/presentation_layer/views/login_view.dart
@@ -1,17 +1,15 @@
-
 import 'package:flutter/material.dart';
 
-
-class LoginView extends StatefulWidget{
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
   @override
   State<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _LoginViewState extends State<LoginView> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold();
   }
 }
