e5724a702d73b3ad956474d2c0d7d138eb47bb75
Mostafaelshrbeny <mostafaelshrbeny6@gmail.com>
2024-04-21 03:01:23 +0200

    pdfNetwork

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,7 @@
 import 'package:bookipedia/app/app_routes.dart';
 import 'package:bookipedia/app/style/app_theme.dart';
+
 import 'package:bookipedia/presentation_layer/screens/auth_screens/login/login_view.dart';
-import 'package:bookipedia/presentation_layer/screens/auth_screens/reset_password/reset_password_view.dart';
-import 'package:bookipedia/presentation_layer/screens/auth_screens/sign_up/sign_up_view.dart';
-import 'package:bookipedia/presentation_layer/screens/home/home_view.dart';
-import 'package:bookipedia/presentation_layer/screens/auth_screens/verify_account/verify_account_view.dart';
 
 import 'package:flutter/material.dart';
 
--- lib/presentation_layer/screens/PdfView/pdf_viewer.dart
+++ lib/presentation_layer/screens/PdfView/pdf_viewer.dart
@@ -13,8 +13,12 @@ class _PdfViewerScreenState extends State<PdfViewerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SfPdfViewer.asset(
-          'assets/أكتر من 60 سؤال شائع في ال Flutter interview.pdf',
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SfPdfViewer.network(
+          'https://powerunit-ju.com/wp-content/uploads/2021/04/Aurelien-Geron-Hands-On-Machine-Learning-with-Scikit-Learn-Keras-and-Tensorflow_-Concepts-Tools-and-Techniques-to-Build-Intelligent-Systems-OReilly-Media-2019.pdf',
           controller: pdfController,
           onAnnotationAdded: (annotation) => {
                 print(
