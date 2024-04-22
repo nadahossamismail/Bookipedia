16e538be3925a145cddfa0237368f6e651c7a80e
Mostafaelshrbeny <mostafaelshrbeny6@gmail.com>
2024-04-22 17:19:37 +0200

    Add document check

--- lib/cubits/UserDocument/user_document_state.dart
+++ lib/cubits/UserDocument/user_document_state.dart
@@ -1,3 +1,9 @@
 abstract class UserDocumentState {}
 
 final class UserDocumentInitial extends UserDocumentState {}
+
+final class UserDocumentLoading extends UserDocumentState {}
+
+final class UserDocumentDone extends UserDocumentState {}
+
+final class UserDocumentFailed extends UserDocumentState {}
